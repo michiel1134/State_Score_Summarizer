9eca6a646b560f431875312be84be080945dacd9
Michiel Besseling <51294666+michiel1134@users.noreply.github.com>
2022-09-29 08:09:10 -0700

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,45 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(data.table)
+library(dplyr) 
+library(tidyverse)
+library(ggplot2)
+df = fread("State Score Summaries.csv")
+df$Group = str_sub(df$Group,5,-1)
+colnames(df) = str_replace(colnames(df), "nmean", "mean")
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel(h2("State Model Score by Key Demographics",  style = "color:blue;")),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("state",
+                        h4("State", style = "color:blue;"),
+                        choices = unique(df$State)),
+            br(),
+            selectInput("score",
+                        h4("Scores", style = "color:blue;"),
+                        choices = c("Intent to Enroll Score",
+                                    "Community College Score",
+                                    "University Score",
+                                    "Vocational or Technical School Score",
+                                    "Hybrid Education Score", 
+                                    "All Online Education Score",
+                                    "Intent to Seek New Job Score"),
+                        ) 
+        ),
+        # Show a plot of the generated distribution
+        mainPanel(
+            tableOutput("df_selected")
+        ))
+))
